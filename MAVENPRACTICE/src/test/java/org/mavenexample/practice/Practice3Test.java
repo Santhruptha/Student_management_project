0ee571af01d64c5588a25f8c19e8a87079205d6a
santh <santh@LAPTOP-8SEKENFG>
2022-07-19 11:43:54 +0530

    1 st commit

---
+++ MAVENPRACTICE/src/test/java/org/mavenexample/practice/Practice3Test.java
@@ -0,0 +1,23 @@
+package org.mavenexample.practice;
+
+import org.testng.annotations.Test;
+
+public class Practice3Test {
+	
+	@Test
+	public void practice1(){
+		System.out.println("test--1-->class3");
+		
+	}
+	@Test
+	public void practice2(){
+		System.out.println("test--2-->class3");
+		
+	}
+	@Test
+	public void practice3(){
+		System.out.println("test--3-->class3");
+		
+	}
+
+}
